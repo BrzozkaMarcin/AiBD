0904aae9621cec7319581591c8d21cb26ce02d95
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2023-01-19 19:16:51 +0100

    Skonczone

--- Lab13/app.py
+++ Lab13/app.py
@@ -1,4 +1,6 @@
+from typing import List
 from textblob import TextBlob
+
 
 def hello(name):
     output = f'Hello {name}'
@@ -10,5 +12,23 @@ def extract_sentiment(text):
 
     return text.sentiment.polarity
 
+
 def text_contain_word(word: str, text: str):
     return word in text
+
+
+# Faza Green i poźniejsza Faza Refactor
+def bubble_sort(lst_original: List[int]) -> List[int]:
+    lst = lst_original[:]
+    n = len(lst)
+    counter = 0
+    change_position = 1
+    while n > 1 and change_position > 0:
+        change_position = 0
+        for i in range(1, n):
+            counter += 1
+            if lst[i - 1] > lst[i]:
+                lst[i - 1], lst[i] = lst[i], lst[i - 1]
+                change_position += 1
+        n -= 1
+    return lst
--- Lab13/test/test_app.py
+++ Lab13/test/test_app.py
@@ -1,6 +1,7 @@
 from app import hello
 from app import extract_sentiment
 from app import text_contain_word
+from app import bubble_sort
 import pytest
 
 
@@ -10,21 +11,36 @@ def test_hello():
 
     assert got == want
 
+
 testdata1 = ["I think today will be a great day"]
+
 
 @pytest.mark.parametrize('sample', testdata1)
 def test_extract_sentiment(sample):
-
     sentiment = extract_sentiment(sample)
 
     assert sentiment > 0
 
+
 testdata2 = [
     ('There is a duck in this text', 'duck', True),
     ('There is nothing here', 'duck', False)
-    ]
+]
+
 
 @pytest.mark.parametrize('sample, word, expected_output', testdata2)
 def test_text_contain_word(sample, word, expected_output):
-
     assert text_contain_word(word, sample) == expected_output
+
+
+testdata3 = [
+    ([1, 3, 5, 2, 4, 6], [1, 2, 3, 4, 5, 6]),
+    ([4, 3, 2, 1, 0], [0, 1, 2, 3, 4]),
+    ([1, 7, 3, 6, 4], [1, 3, 4, 6, 7]),
+    ([6, 3, 2, 6, 0, 0], [0, 0, 2, 3, 6, 6])
+]
+
+# Faza red
+@pytest.mark.parametrize('sample, sorted_sample', testdata3)
+def test_bubble_sort(sample, sorted_sample):
+    assert bubble_sort(sample) == sorted_sample
